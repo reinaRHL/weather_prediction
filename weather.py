66acef02fa766b736a5438b6dc03412f6a9446e3
reina <reinal@sfu.ca>
2017-08-06 01:16:07 -0700

    cleaning data category

--- weather.py
+++ weather.py
@@ -10,20 +10,35 @@ import glob
 from os import getcwd
 from os.path import join, abspath
 
-
+def simplify_cat(weather):
+    #simplify (clean) category
+    weather = weather.replace('Moderate Snow','Snow')
+    weather = weather.replace('Freezing Fog','Fog')
+    weather = weather.replace('Freezing Rain','Rain')
+    weather = weather.replace('Moderate Rain Showers','Rain Showers')
+    weather = weather.replace('Moderate Rain','Rain')
+    weather = weather.replace('Mainly Clear','Clear')
+    weather = weather.replace('Moderate Snow','Snow')
+    weather = weather.replace('Mostly Cloudy','Cloudy')
+    weather = weather.replace('Snow Pellets','Ice Pellets')
+    return weather
 
 def main():
+
+    # reading multiple files in the same folder. 
+    #Code is from https://stackoverflow.com/questions/39568925/python-read-files-from-directory-and-concatenate-that
     path ='/yvr-weather' # use your path
     allFiles = glob.glob(abspath(getcwd())+ path + "/*.csv")
-    data = pd.DataFrame()
+    frame = pd.DataFrame()
     list_ = []
     for file_ in allFiles:
         df = pd.read_csv(file_, header=14)
         list_.append(df)
-    data = pd.concat(list_, ignore_index=True)
-    print (data)
-      
-
+    frame = pd.concat(list_, ignore_index=True)
+    
+    frame = frame[frame['Weather'].notnull()]
+    frame['simple cat'] = frame['Weather'].apply(simplify_cat)  
+    print (frame.groupby(['simple cat'])['Temp (°C)'].mean())
 
 
 if __name__=='__main__':
