72b983b2b8b70b4389a80041d1dcf069635c497e
reina <reinal@sfu.ca>
2017-08-07 21:59:01 -0700

    added comments

--- tide.py
+++ tide.py
@@ -3,33 +3,26 @@
 # CMPT 318 Final Project
 ####################################################
 
-from timeit import default_timer as timer
 import pandas as pd
 import numpy as np
 import sys
-import gzip
-import math as Math
-import matplotlib.pyplot as plt
-from datetime import datetime as dt
+from timeit import default_timer as timer
 from scipy import stats, misc
 import glob
 import os
 from os import getcwd
 from os.path import join, abspath
 import csv
-from sklearn.multioutput import MultiOutputClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import FunctionTransformer, StandardScaler
+from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
-from sklearn.multiclass import OneVsOneClassifier, OneVsRestClassifier
-from sklearn.svm import LinearSVC
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 
 
 
-def classify(data, avg, std):
+def classify_tide(data, avg, std):
     if data < avg - std:
         return 'low'
     elif data >= avg-std and data < avg+std:
@@ -52,7 +45,8 @@ def classify_time(time):
     else:
         return 'night'
 
-# Extract date (YYYYmmDD-HH) from the filepath
+
+# Extract date from the filepath
 def getDate(filepath):
     date = os.path.basename(filepath)
     date = os.path.splitext(date)[0]
@@ -80,21 +74,22 @@ def datetime(t):
     return date_final
 
 
-
-
-    ##################################################################################
-    ############## READ INPUT FILE - weather data and image data #####################
-    ##################################################################################
 def main():
-    start = timer()
+
+    ########################################################################################
+    ############## READ FILE and Clean Data - tide data and image data #####################
+    ########################################################################################
+    
     tide_df = pd.read_csv('tideData.csv')
+
+    # Clean the tide height data
     tide_df = pd.DataFrame(tide_df, columns=['TIME_TAG PST (Z+8)', 'ENCODER1'])
     tide_df.columns = ['Date/Time', 'Tide']
     tide_df['Date/Time'] = tide_df['Date/Time'].apply(datetime)
     avg_tide = tide_df['Tide'].mean()
     std_tide = tide_df['Tide'].std()
     tide_df_new = tide_df.groupby(tide_df['Date/Time']).mean()
-    tide_df_new['Tide'] = tide_df_new['Tide'].astype(float).apply(classify,  args = (avg_tide, std_tide,))
+    tide_df_new['Tide'] = tide_df_new['Tide'].astype(float).apply(classify_tide,  args = (avg_tide, std_tide,))
     tide_df_new = tide_df_new.reset_index()
     tide_df_new["year"]= tide_df_new['Date/Time'].apply(year)
     tide_df_new["month"]= tide_df_new['Date/Time'].apply(month)
@@ -102,19 +97,18 @@ def main():
     tide_df_new["time"]= tide_df_new['Date/Time'].apply(time)
     tide_df_new['time'] = tide_df_new['time'].astype(int).apply(classify_time)
 
+    # read image file from the folder
     # idea from https://stackoverflow.com/questions/39165992/converting-appended-images-list-in-pandas-dataframe
     path ='/katkam-scaled_700'
     allFiles = glob.glob(abspath(getcwd())+ path + "/*.jpg")
     list1_ = []
     list2_ = []
     image_date = pd.DataFrame()
-    print ("Reading image file...")
     for file_ in allFiles:
         list1_.append(os.path.basename(file_))
         list2_.append(misc.imread(file_).reshape(-1))
-    print ("read image file...")
 
-    # image_date contains 'Date/Time' info
+    # image_date contains 'Date/Time' of the image
     image_date= pd.DataFrame(list1_, columns=['Date/Time'])
     image_date['Date/Time'] = image_date['Date/Time'].apply(getDate)
 
@@ -122,18 +116,12 @@ def main():
     image_df = pd.DataFrame.from_records(list2_)
 
     # Combine Date/time columns and image data
-    # and set index as 'Date/Time' so that we can merge this df with weather data on this feature. 
+    # and set index as 'Date/Time' so that we can merge this df with tide data on this feature. 
     image_df = pd.concat([image_date, image_df], axis=1)
     image_df.set_index('Date/Time')
 
-
-    ####################################################
-    ############## Cleaning Data   #####################
-    ####################################################
-
-    # Merge image dataframe and weather data frame 
-    # It will keep rows that has matching data from both dataFrame
-  
+    # Merge image dataframe and tide data frame 
+    # It will keep rows that has matching date/time from both dataFrame
     tide_df_new = tide_df_new.merge(image_df, on='Date/Time')
 
 
@@ -142,51 +130,60 @@ def main():
     # ################################################################
 
     #Build models for tide prediction
-    print("model start")
 
-    gsModel_tide = make_pipeline(StandardScaler(), GaussianNB())
+    #gsModel_tide = make_pipeline(StandardScaler(), GaussianNB())
 
-    knn_model_tide = make_pipeline(StandardScaler(), KNeighborsClassifier(n_neighbors=11))
+    #knn_model_tide = make_pipeline(StandardScaler(), KNeighborsClassifier(n_neighbors=11))
     
+    # svc model is most accurate for this data
     svc_model_tide = make_pipeline(StandardScaler(), SVC())
 
+    # input data
     X = tide_df_new[tide_df_new.columns[6:147463]]
+
+    # target data, tide height is one of 'low', 'medium', 'high'
     y = tide_df_new['Tide'].values
+    
     X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-    gsModel_tide.fit(X_train, y_train)
-    knn_model_tide.fit(X_train, y_train)
+    #gsModel_tide.fit(X_train, y_train)
+    #knn_model_tide.fit(X_train, y_train)
     svc_model_tide.fit(X_train, y_train)
 
     print ("Accuracy score for tide height prediction:")
-    print ("Gaussian-")
-    print (gsModel_tide.score(X_test, y_test))
-    print ("KNN-")
-    print (knn_model_tide.score(X_test, y_test))
-    print ("SVC-")
+    # print ("Gaussian-")
+    # print (gsModel_tide.score(X_test, y_test))
+    # print ("KNN-")
+    # print (knn_model_tide.score(X_test, y_test))
     print (svc_model_tide.score(X_test, y_test))
 
 
-    # Build a model for time
-    gsModel_time = GaussianNB()
-    knn_model_time = KNeighborsClassifier(n_neighbors=11)
-    svc_model_time = SVC()
+    # Build a model for time prediction
+    
+    #gsModel_time = GaussianNB()
 
+    # This time, knn was the most accurate model
+    knn_model_time = KNeighborsClassifier(n_neighbors=11)
+    
+    #svc_model_time = SVC()
+
+    # input
     X = tide_df_new[tide_df_new.columns[6:147463]]
+    
+    # target, time is one of 'morning', 'afternoon', 'evening', 'night'
     y = tide_df_new['time'].values
     X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-    gsModel_time.fit(X_train, y_train)
+    #gsModel_time.fit(X_train, y_train)
     knn_model_time.fit(X_train, y_train)
-    svc_model_time.fit(X_train, y_train)
+    #svc_model_time.fit(X_train, y_train)
 
     print ("Accuracy score for time prediction:")
-    print ("Gaussian-")
-    print (gsModel_time.score(X_test, y_test))
-    print ("KNN-")
+    # print ("Gaussian-")
+    # print (gsModel_time.score(X_test, y_test))
     print (knn_model_time.score(X_test, y_test))
-    print ("SVC-")
-    print (svc_model_time.score(X_test, y_test))
+    # print ("SVC-")
+    # print (svc_model_time.score(X_test, y_test))
 
 
     # ####################################################
@@ -214,8 +211,6 @@ def main():
     # predictions = svc_model.predict(X_pre)
     # print ("svc")
     # print (predictions)
-    end = timer()
-    print (end-start)
 
 
 
